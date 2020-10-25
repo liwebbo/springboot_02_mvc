b7ce7f99506ec9eed387e017c732e325d5a95622
liwenbo <2844696789@qq.com>
2020-10-25 19:19:13 +0800

    创建类

---
+++ src/main/java/com/yaofande/GitTest.java
@@ -0,0 +1,10 @@
+package com.yaofande;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123456789");
+        System.out.println("qqqqq9");
+        System.out.println("sss89");
+        System.out.println("fffff789");
+    }
+}
